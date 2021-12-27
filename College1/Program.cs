0019dc032c5d5b43736bf981094ac73ae848da18
Mahmoud El Gohary <61092710+Mahmoud-El-Gohary@users.noreply.github.com>
2021-12-27 17:24:48 +0200

    Change 97
    form :  for (int possiableFactors = 1; possiableFactors < number ; possiableFactors++)
    to :      for (int possiableFactors = 1; possiableFactors <= number/2 ; possiableFactors++)
    science there is no factors in the range [number/2 - number]

--- College1/Program.cs
+++ College1/Program.cs
@@ -94,7 +94,7 @@ namespace College1
                     // it is written outside the loop so would be wxcuted juts once 
                     Console.Write("[+] {0} Factors are:", number);
                     // looping for each number less than given number to get all possible factors
-                    for (int possiableFactors = 1; possiableFactors < number; possiableFactors++)
+                    for (int possiableFactors = 1; possiableFactors <= number/2 ; possiableFactors++)
                     {
                         // using the factor definiation to check for all real factors as if we divide the number by its factor there will be no reminder 
                         if (number % possiableFactors == 0)
